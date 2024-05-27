74e0ce6022d56577cb196b55612ffd8f4db5c840
wambugut <wambuguthomas01@gmail.com>
2024-05-27 19:00:17 +0300

    Fixed Home component export and added images

--- pages/Home.js
+++ pages/Home.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import dollarsImage from '../assets/dollars.jpg';
+import basketballImage from '../assets/basketball.jpg';
+import evolutionImage from '../assets/evolution.jpg';
 
-    const Home = () => {
-        return (
-            <div className="container">
-                <h1>Home Page</h1>
-                <div className="row">
-                    <div className="col-md-4">
-                        <img src="https://via.placeholder.com/150" alt="Placeholder" className="img-fluid"/>
-                    </div>
-                    <div className="col-md-4">
-                        <img src="https://via.placeholder.com/150" alt="Placeholder" className="img-fluid"/>
-                    </div>
-                    <div className="col-md-4">
-                        <img src="https://via.placeholder.com/150" alt="Placeholder" className="img-fluid"/>
-                    </div>
-                </div>
-            </div>
-        );
-    };
+const Home = () => {
+  return (
+    <div className="container">
+      <h1>Home Page</h1>
+      <div className="row">
+        <div className="col-md-4">
+          <img src={dollarsImage} alt="Dollars" className="img-fluid" />
+        </div>
+        <div className="col-md-4">
+          <img src={basketballImage} alt="Basketball" className="img-fluid" />
+        </div>
+        <div className="col-md-4">
+          <img src={evolutionImage} alt="Evolution" className="img-fluid" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    export default Home;
+export default Home;
